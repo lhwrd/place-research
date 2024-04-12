ed68c7f20792c95032e0a528a0a072afae31c6c6
Lucas Howard <lhoward96@gmail.com>
2024-04-11 20:13:31 -0400

    Update client

--- place_research/client.py
+++ place_research/client.py
@@ -2,8 +2,9 @@
 import os
 from googlemaps import Client
 
+
 def get_client(api_key: str | None = None):
-    """ Get a Google Maps API client instance."""
+    """Get a Google Maps API client instance."""
     api_key = api_key or os.environ.get("GOOGLE_MAPS_API_KEY")
     assert api_key is not None, "API key must be provided."
     return Client(key=api_key, timeout=10, queries_per_second=5)
